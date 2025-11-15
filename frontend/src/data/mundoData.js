c95310b033d2ae5a9c91e277563299346395615e
Anna Caroline <carolannago2005@gmail.com>
2025-11-15 18:01:11 -0300

    feat: adicionar caminho de audio em mundo Data.js

--- frontend/src/data/mundoData.js
+++ frontend/src/data/mundoData.js
@@ -10,18 +10,22 @@ export const mundos = {
       {
         imagem: "/monkey-story-1.svg",
         dialogo: "Oi! Eu sou o Abú! O macaquinho mais sapeca da floresta!",
+        audio: "/monkey-audio-1.mp3",
       },
       {
         imagem: "/monkey-story-2.svg",
         dialogo: "Fiquei tão animado com a festa na selva que saí pulando de galho em galho pra colher as frutas mais docinhas!",
+        audio: "/monkey-audio-2.mp3",
       },
       {
         imagem: "/monkey-story-3.svg",
         dialogo: "Mas... Adivinha só? No caminho algumas letrinhas caíram dos nomes das frutas!",
+        audio: "/monkey-audio-3.mp3",
       },
       {
         imagem: "/monkey-story-4.svg",
         dialogo: "Pode me ajudar a juntar tudo de novo? Assim nossa festa vai ficar deliciosa e super divertida!",
+        audio: "/monkey-audio-4.mp3",
       },
     ]
     },
@@ -35,18 +39,22 @@ export const mundos = {
       {
         imagem: "/bear-story-1.svg",
         dialogo: "Oi! Eu sou o Polado! Vim da minha casa geladinha só para curtir a festa na selva!",
+        audio: "/bear-audio-1.mp3",
       },
       {
         imagem: "/bear-story-2.svg",
         dialogo: "Como eu entendo tudo de coisas refrescantes, fiquei responsável por trazer as bebidas mais gostosas!",
+        audio: "/bear-audio-2.mp3",
       },
       {
         imagem: "/bear-story-3.svg",
         dialogo: "Mas... Durante a viagem, as etiquetas se misturaram todinhas!",
+        audio: "/bear-audio-3.mp3",
       },
       {
         imagem: "/bear-story-4.svg",
         dialogo: "Pode me ajudar a colocar as letrinhas em ordem? Assim todo mundo vai saber qual bebida escolher!",
+        audio: "/bear-audio-4.mp3",
       },
     ]
 },
@@ -60,18 +68,22 @@ export const mundos = {
       {
         imagem: "/frog-story-1.svg",
         dialogo: "Oi! Eu sou o Hebert! O sapo mais criativo da lagoa! Preparei fitas, flores e enfeites para a festa na selva!",
+        audio: "/frog-audio-1.mp3",
       },
       {
         imagem: "/frog-story-2.svg",
         dialogo: "Saltei de vitória-régia em vitória-régia para enfeitar tudinho! Mas, ops... Minhas anotações se misturaram!",
+        audio: "/frog-audio-2.mp3",
       },
       {
         imagem: "/frog-story-3.svg",
         dialogo: "As palavras dos enfeites voaram e ficaram embaralhadas! Agora não sei mais o nome de cada decoração...",
+        audio: "/frog-audio-3.mp3",
       },
       {
         imagem: "/frog-story-4.svg",
         dialogo: "Você pode me ajudar a desembaralhar as palavras? Assim a festa vai ficar linda e brilhante!",
+        audio: "/frog-audio-4.mp3",
       },
     ]
 },
@@ -85,18 +97,22 @@ export const mundos = {
       {
         imagem: "/tiger-story-1.svg",
         dialogo: "Oi, eu sou o Tao! O tigre mais corajoso e imponente da savana!",
+        audio: "/tiger-audio-1.mp3",
       },
       {
         imagem: "/tiger-story-2.svg",
-        dialogo: "Eu vou trazer as sobremesas mais gostosas da Festa na Selva! bolos, pudins e brigadeiros e mais!",
+        dialogo: "Eu vou trazer as sobremesas mais gostosas da Festa na Selva! bolos, pudins, brigadeiros e mais!",
+        audio: "/tiger-audio-2.mp3",
       },
       {
         imagem: "/tiger-story-3.svg",
         dialogo: "Mas no caminho... as palavras com os nomes das sobremesas se esconderam entre as letras da savana...",
+        audio: "/tiger-audio-3.mp3",
       },
       {
         imagem: "/tiger-story-4.svg",
         dialogo: "Você me ajuda a encontrar todas as palavras? Assim nossa mesa vai ficar docinha e pronta pra festa!",
+        audio: "/tiger-audio-4.mp3",
       },
     ]
 },
@@ -109,19 +125,23 @@ export const mundos = {
     historia: [
       {
         imagem: "/fox-story-1.svg",
-        dialogo: "Oi, eu sou a Luma! A raposa mais curiosa e esperta da tundra!",
+        dialogo: "Oi, eu sou a Faísca! A raposa mais curiosa e esperta do bosque!",
+        audio: "/fox-audio-1.mp3",
       },
       {
         imagem: "/fox-story-2.svg",
         dialogo: "A Festa da Selva está chegando, e eu fiquei responsável por lembrar de tudinho!",
+        audio: "/fox-audio-2.mp3",
       },
       {
         imagem: "/fox-story-3.svg",
         dialogo: "Mas... ai, ai... Acabei misturando todos os presentes no meio da grama!",
+        audio: "/fox-audio-3.mp3",
       },
       {
         imagem: "/fox-story-4.svg",
         dialogo: "Você me ajuda a encontrar os pares iguais?  Assim tudo vai ficar prontinho pra grande festa!",
+        audio: "/fox-audio-4.mp3",
       },
     ]
 },
